57818714adbeb461c75bcdb5c187d6d2b1ad504e
angelashamonina <76558153+angelashamonina@users.noreply.github.com>
2021-10-16 16:21:38 +0300

    Update test_tset.cpp

--- test/test_tset.cpp
+++ test/test_tset.cpp
@@ -295,3 +295,29 @@ TEST(TSet, check_negation_operator)
 
   EXPECT_EQ(expSet, set1);
 }
+TEST(TSet, can_combine_three_sets)
+{
+	const int size = 7;
+	TSet set1(size), set2(size), set3(size), expSet(size), set4(size);
+	// set1 = {3, 2, 6}
+	set1.InsElem(3);
+	set1.InsElem(2);
+	set1.InsElem(6);
+	// set2 = {0, 1, 2}
+	set2.InsElem(0);
+	set2.InsElem(1);
+	set2.InsElem(2);
+	// set3= { 0, 2, 4}
+	set3.InsElem(0);
+	set3.InsElem(2);
+	set3.InsElem(4);
+	set4 = set1 + set2 + set3;
+	// expSet = {0, 1, 2,3, 4,6}
+	expSet.InsElem(0);
+	expSet.InsElem(1);
+	expSet.InsElem(2);
+	expSet.InsElem(3);
+	expSet.InsElem(4);
+	expSet.InsElem(6);
+	EXPECT_EQ(expSet, set4);
+}
